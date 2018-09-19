90d4204709542cea82402c2927ee80ae298338e1
felipekenzo <felipe.kenzo@live.com>
2018-09-19 18:47:30 -0300

    First Commit

---
+++ Assets/Scripts/FlappyBird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBird : MonoBehaviour {
+
+	public Sprite newSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 500));
+		}
+
+	}
+}
