94de2d837f5293997c0f477a807b3415f75e0654
leandORaumada <43447648+leandORaumada@users.noreply.github.com>
2018-09-20 23:45:26 -0300

    Add files via upload

--- Assets/Scripts/FlappyBird.cs
+++ Assets/Scripts/FlappyBird.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class FlappyBird : MonoBehaviour {
 
-	public Sprite newSprite;
+    public GameObject Bird;
+    private Sprite[] spritesList;
+    private int contador;
+    private bool isVoando;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void Start ()
+    {
+        //Vetor com os sprites em. OBS: Ele apenas carregas os sprites da pasta Resources
+        spritesList = Resources.LoadAll<Sprite>("");
+        contador = 0;
+        isVoando = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space)) {
-			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 500));
-		}
+	void Update ()
+    {
+
+        //Lógica da Animação
+        if (isVoando && contador < 20)
+        {
+            MudaSkin(0);
+            contador++;
+        } else if(isVoando && contador >= 20 && contador < 40 ){
+            MudaSkin(2);
+            contador++;
+        } else if(isVoando && contador >= 40){
+            MudaSkin(1);
+            contador = 0;
+            isVoando = false;
+        }
+
+
+		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space))
+        {
+			Bird.GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, 500));   
+
+            //Inicia a animação         
+            MudaSkin(2);
+            isVoando = true;
+            
+        }
 
 	}
+
+    //Muda  o Sprite do Bird
+    void MudaSkin(int i)
+    {
+        Bird.GetComponent<SpriteRenderer>().sprite = spritesList[i];
+    }
 }
